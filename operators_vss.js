469c64e226a924971b56cb6aceefa2da024b9f55
Sruthi.R <67775776+Sruthi-19@users.noreply.github.com>
2022-10-21 17:26:30 +0530

    Add files via upload

---
+++ operators_vss.js
@@ -0,0 +1,52 @@
+let a=14;
+let b=3;
+let c='3';
+let d=14;
+
+
+// ARITHMETIC OPERATORS
+console.log("ADDITION",a+b);
+console.log("SUBTRACTION",a-b);
+console.log("MULTIPLICATION",a*b);
+console.log("DIVISION",a/b);
+console.log("MODULO DIVISION",a%b);
+console.log("POWER",a**2);
+
+
+// COMPARISON OPERATORS
+console.log("GREATER THAN",a>b);
+console.log("GREATER THAN OR EQUAL TO",a>=b);
+console.log("LESSER THAN OR EQUAL TO",a<=d);
+console.log("EQUALITY ONLY DATA",b==c);
+console.log("EQUALITY BOTH DATA AND DATA TYPE IS CHECKED",b===c);
+
+
+// BITWISE OPERATOR
+console.log("BITWISE AND",a&b);
+console.log("BITWISE OR",a|b);
+
+
+// LOGICAL OPERATOR
+console.log("LOGICAL AND",a&&b);
+console.log("LOGICAL OR",a||b);
+
+
+// ASSIGNMENT OPERATOR
+console.log("STORES variable 'a' as a=a+b: ",a+=b); 
+console.log("STORES variable 'a' as a=a-b: ",a-=b); 
+console.log("STORES variable 'a' as a=a*b: ",a*=b); 
+console.log("STORES variable 'a' as a=a/b: ",a/=b); 
+console.log("STORES variable 'a' as a=a%b: ",a%=b);
+
+// TERNARY OPERATOR
+const a1=15;
+const b1=3;
+var ter=a1>b1 ? "a1 is greater than b1" : "a1 is smaller than b1";
+console.log(ter);
+
+
+
+
+
+
+   
